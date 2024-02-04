fdfd569efab9ab3bd1463542ec0b4dbb1788e2b2
script-js <134544514+script-js@users.noreply.github.com>
2024-02-03 17:29:30 -0800

    Update openNewTab.js

--- openNewTab.js
+++ openNewTab.js
@@ -1,6 +1,10 @@
 var ntDIV = document.createElement("div")
+var sslink = document.createElement("link")
+sslink.href = "https://script-js.github.io/js-css/openNewTab.css";
+sslink.rel = "stylesheet";
 ntDIV.classList = "openNew";
 ntDIV.innerHTML = "open_in_new";
 ntDIV.onmouseover = function() {this.innerHTML = "<span style='font-family:sans-serif'>Open in New Tab</span>open_in_new";}
 ntDIV.onmouseout = function() {this.innerHTML = "open_in_new";}
+document.body.appendChild(sslink)
 document.body.appendChild(ntDIV)

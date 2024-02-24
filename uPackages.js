25ddbfcdc538cbd22cb418295920641917b7dd06
script-js <134544514+script-js@users.noreply.github.com>
2024-02-23 19:33:38 -0800

    Update uPackages.js

--- uPackages.js
+++ uPackages.js
@@ -1,6 +1,6 @@
 var packages = {
-  "Runner": ["The Original: run any JavaScript","eval(prompt('Type Code:'))"],
-  "Page Editor (On)": ["Edit any page's contents","document.designMode = 'on'"],
-  "Page Editor (Off)": ["Disable Page Editor","document.designMode = 'off'"],
-  "Embed It!": ["Embed almost any webpage in another",atob("ZG9jdW1lbnQud3JpdGUoJzx0aXRsZT4nICsgZG9jdW1lbnQudGl0bGUgKyAnPC90aXRsZT48aWZyYW1lIHNyYz0iJyArIHByb21wdCgiVVJMPyIpICsgJyIgc3R5bGU9ImJvcmRlcjogbm9uZTsgd2lkdGg6MTAwJTtoZWlnaHQ6IDEwMCU7cG9zaXRpb246YWJzb2x1dGU7bGVmdDowcHg7dG9wOjBweDsiIGFsbG93ZnVsbHNjcmVlbj4nKQ==")]
+  "Runner": ["The Original: run any JavaScript",btoa("eval(prompt('Type Code:'))")],
+  "Page Editor (On)": ["Edit any page's contents",btoa("document.designMode = 'on'")],
+  "Page Editor (Off)": ["Disable Page Editor",btoa("document.designMode = 'off'")],
+  "Embed It!": ["Embed almost any webpage in another","ZG9jdW1lbnQud3JpdGUoJzx0aXRsZT4nICsgZG9jdW1lbnQudGl0bGUgKyAnPC90aXRsZT48aWZyYW1lIHNyYz0iJyArIHByb21wdCgiVVJMPyIpICsgJyIgc3R5bGU9ImJvcmRlcjogbm9uZTsgd2lkdGg6MTAwJTtoZWlnaHQ6IDEwMCU7cG9zaXRpb246YWJzb2x1dGU7bGVmdDowcHg7dG9wOjBweDsiIGFsbG93ZnVsbHNjcmVlbj4nKQ=="]
 }

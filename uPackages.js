2dd04f5a7c4977880bb67e94d38d5c2d82f50b20
script-js <134544514+script-js@users.noreply.github.com>
2024-02-24 17:44:22 -0800

    Update uPackages.js

--- uPackages.js
+++ uPackages.js
@@ -3,7 +3,6 @@ var packages = {
   "Page Editor (On)": "Edit any page's contents",
   "Page Editor (Off)": "Disable Page Editor",
   "Embed It!": "Make almost any webpage another",
-  "Change Title": "Change the title of a website",
   "Canfetti": "Add confetti to a canvas assignment page",
   "PlainPage": "Remove all stylesheets from websites",
   "webdestroyer": "Destroy webpages with a simple stylesheet",

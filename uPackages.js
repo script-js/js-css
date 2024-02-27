aaaaf379cdb92ffaf24097bc880b6648b630da3a
script-js <134544514+script-js@users.noreply.github.com>
2024-02-26 16:12:12 -0800

    Update uPackages.js

--- uPackages.js
+++ uPackages.js
@@ -1,10 +1,11 @@
 var packages = {
-  "Runner": "The Original: run any JavaScript",
-  "Page Editor": "Edit any page's contents",
-  "Embed It!": "Make almost any webpage another",
-  "Canfetti": "Add confetti to a canvas assignment page",
-  "PlainPage": "Remove all stylesheets from websites",
-  "webdestroyer": "Destroy webpages with a simple stylesheet",
-  "die-Ready": "Destroy i-Ready",
-  "Full Screen Hider, from Hapara Hider": "Click a button to open a website"
+  "Page Editor On": ["Edit any page's contents","document.designMode = 'on'"],
+  "Page Editor Off": ["Edit any page's contents","document.designMode = 'off'"],
+  "Embed It!": ["Make almost any webpage another",atob("ZG9jdW1lbnQud3JpdGUoJzx0aXRsZT4nICsgZG9jdW1lbnQudGl0bGUgKyAnPC90aXRsZT48aWZyYW1lIHNyYz0iJyArIHByb21wdCgiVVJMIChhZGQgLy8pOiIpICsgJyIgc3R5bGU9ImJvcmRlcjogbm9uZTsgd2lkdGg6MTAwJTtoZWlnaHQ6IDEwMCU7cG9zaXRpb246YWJzb2x1dGU7bGVmdDowcHg7dG9wOjBweDsiIGFsbG93ZnVsbHNjcmVlbj4nKQ==")],
+  "Canfetti": ["Add confetti to a canvas assignment page","location.replace(location.href + '?confetti=true')"],
+  "PlainPage": ["Remove all stylesheets from websites","void(document.getElementsByTagName('head')[0].innerHTML = '')"],
+  "webdestroyer": ["Destroy webpages with a simple stylesheet",atob("dm9pZChkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaGVhZCIpWzBdLmlubmVySFRNTCA9ICI8bGluayByZWw9J3N0eWxlc2hlZXQnIGhyZWY9J2h0dHBzOi8vY3RleHBlcmltZW50cy5wYWdlcy5kZXYvYm9va21hcmtoYWNrcy9jb2xvcnN3aXRjaGVyLmNzcyc+PHRpdGxlPiIgKyBkb2N1bWVudC50aXRsZSArICIsIGRlc3Ryb3llZCBieSB3ZWJkZXN0cm95ZXI8L3RpdGxlPiIp")],
+  "die-Ready": ["Destroy i-Ready",atob("dm9pZChkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaGVhZCIpWzBdLmlubmVySFRNTCA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF0uaW5uZXJIVE1MICsgIjxsaW5rIHJlbD0nc3R5bGVzaGVldCcgaHJlZj0naHR0cHM6Ly9jdGV4cGVyaW1lbnRzLnBhZ2VzLmRldi9ib29rbWFya2hhY2tzL2NvbG9yc3dpdGNoZXIuY3NzJz48dGl0bGU+ZGllLVJlYWR5PC90aXRsZT4iKQ==")],
+  "Full Screen Hider, from Hapara Hider": ["Click a button to open a website",atob("ZG9jdW1lbnQuYm9keS5pbnNlcnRBZGphY2VudEhUTUwoJ2JlZm9yZWVuZCcsIjxicj48YnV0dG9uIHN0eWxlPSdwb3NpdGlvbjphYnNvbHV0ZTsgbGVmdDoxcHg7IHRvcDogMXB4Oycgb25jbGljaz0ndWJyb3dzZXJfZnJhbWUucmVxdWVzdEZ1bGxzY3JlZW4oKSc+T3BlbiBGU0g8L2J1dHRvbj48aWZyYW1lIGlkPSd1YnJvd3Nlcl9mcmFtZScgc3JjPSciICsgcHJvbXB0KCJVUkwgKGFkZCAvLyk6IikgKyAiJyBzdHlsZT0nd2lkdGg6MXB4O2hlaWdodDoxcHg7Jz48L2lmcmFtZT4iKQ==")],
+  "Full Screen Hider: Change Website": ["Change Full Screen Hider website",btoa("void(ubrowser_frame.src = prompt('URL (add //):'))")]
 }

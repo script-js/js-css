f0388eaa1e9269d7aae49e296ec7362ae15c02a3
script-js <134544514+script-js@users.noreply.github.com>
2024-03-07 09:02:25 -0800

    Update uPackages.js

--- uPackages.js
+++ uPackages.js
@@ -6,6 +6,8 @@ var packages = {
   "PlainPage": ["Remove all stylesheets from websites","void(document.getElementsByTagName('head')[0].innerHTML = '')"],
   "webdestroyer": ["Destroy webpages with a simple stylesheet",atob("dm9pZChkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaGVhZCIpWzBdLmlubmVySFRNTCA9ICI8bGluayByZWw9J3N0eWxlc2hlZXQnIGhyZWY9J2h0dHBzOi8vY3RleHBlcmltZW50cy5wYWdlcy5kZXYvYm9va21hcmtoYWNrcy9jb2xvcnN3aXRjaGVyLmNzcyc+PHRpdGxlPiIgKyBkb2N1bWVudC50aXRsZSArICIsIGRlc3Ryb3llZCBieSB3ZWJkZXN0cm95ZXI8L3RpdGxlPiIp")],
   "die-Ready": ["Destroy i-Ready",atob("dm9pZChkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaGVhZCIpWzBdLmlubmVySFRNTCA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF0uaW5uZXJIVE1MICsgIjxsaW5rIHJlbD0nc3R5bGVzaGVldCcgaHJlZj0naHR0cHM6Ly9jdGV4cGVyaW1lbnRzLnBhZ2VzLmRldi9ib29rbWFya2hhY2tzL2NvbG9yc3dpdGNoZXIuY3NzJz48dGl0bGU+ZGllLVJlYWR5PC90aXRsZT4iKQ==")],
+  "Hapara Hider: Change Title": ["Change the title of almost any website","void(document.title = prompt('Title?'))"],
+  "Hapara Hider: Change Icon": ["Change the favicon of almost any website",atob("dm9pZChkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCdsaW5rW3JlbH49Imljb24iXScpLmhyZWYgPSBwcm9tcHQoJ0ljb24gVVJMJykp")],
   "Full Screen Hider, from Hapara Hider": ["Click a button to open a website",atob("ZG9jdW1lbnQuYm9keS5pbnNlcnRBZGphY2VudEhUTUwoJ2JlZm9yZWVuZCcsIjxicj48YnV0dG9uIHN0eWxlPSdwb3NpdGlvbjphYnNvbHV0ZTsgbGVmdDoxcHg7IHRvcDogMXB4Oycgb25jbGljaz0ndWJyb3dzZXJfZnJhbWUucmVxdWVzdEZ1bGxzY3JlZW4oKSc+T3BlbiBGU0g8L2J1dHRvbj48aWZyYW1lIGlkPSd1YnJvd3Nlcl9mcmFtZScgc3JjPSciICsgcHJvbXB0KCJVUkwgKGFkZCAvLyk6IikgKyAiJyBzdHlsZT0nd2lkdGg6MXB4O2hlaWdodDoxcHg7Jz48L2lmcmFtZT4iKQ==")],
   "Full Screen Hider: Change Website": ["Change Full Screen Hider website","void(ubrowser_frame.src = prompt('URL (add //):'))"]
 }

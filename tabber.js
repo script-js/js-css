e434b71d70fc5f56cd4a151a9c068decd8c4f888
script-js <134544514+script-js@users.noreply.github.com>
2024-03-15 15:13:03 -0700

    Update tabber.js

--- tabber.js
+++ tabber.js
@@ -518,7 +518,7 @@ document.cookie = "oldsmobile=badcar"
 
 
 function titleChanger(id) {
-  document.getElementsByClassName("2")[0].innerText = prompt("Tab Title:")
+  document.getElementsByClassName(id)[0].innerText = prompt("Tab Title:")
 }
 
 

8d19e990e81e6e10c314d5c8165dc3d75129bf53
script-js <134544514+script-js@users.noreply.github.com>
2024-03-15 15:06:19 -0700

    Update tabber.js

--- tabber.js
+++ tabber.js
@@ -232,7 +232,7 @@ document.cookie = "oldsmobile=badcar"
 
     createNewTabEl() {
       const div = document.createElement("div");
-      div.innerHTML = `<div ifd="${tabC++}"class="chrome-tab" onclick="opencity('${tabC}')">
+      div.innerHTML = `<div ifd="${tabC++}"class="chrome-tab" onclick="opencity('${tabC}')" ondblclick="titleChanger('${tabC}')">
               <div class="chrome-tab-dividers"></div>
               <div class="chrome-tab-background">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="chrome-tab-geometry-left" viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z"/></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlink:href="#chrome-tab-geometry-left"/></symbol><clipPath id="crop"><rect class="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="52%" height="100%"><use xlink:href="#chrome-tab-geometry-left" width="214" height="36" class="chrome-tab-geometry"/></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlink:href="#chrome-tab-geometry-right" width="214" height="36" class="chrome-tab-geometry"/></svg></g></svg>
@@ -517,7 +517,9 @@ document.cookie = "oldsmobile=badcar"
 });
 
 
-
+function titleChanger(id) {
+  document.querySelector("." + id).innerText = prompt("Tab Title:")
+}
 
 
 

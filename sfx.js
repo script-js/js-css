baebe0bbb044dcfee878ce6851d5c207ff5946bc
script-js <134544514+script-js@users.noreply.github.com>
2024-07-19 09:19:08 -0700

    sfx

---
+++ sfx.js
@@ -0,0 +1,24 @@
+var sfx = {
+  play: function(url) {
+    var sfxPlayer = new Audio(url);
+    sfxPlayer.addEventListener("canplaythrough", (event) => {
+  sfxPlayer.play();
+});
+
+  },
+  addMenuSounds: function(url) {
+    var elems = document.querySelectorAll("button")
+    var addfx = function() {
+    Object.keys(elems).forEach(function(k) {
+      elems[k].addEventListener("click",function() {
+        sfx.play(url)
+      }
+    }
+    }
+    addfx()
+    const observer = (new MutationObserver(addfx)).observe(document.body, {
+  subtree: true,
+  childList: true,
+});
+  }
+}
